2d6864b582494526212f3f4c78feb4e777ad4947
Dilara <50795069+aralids@users.noreply.github.com>
2024-08-10 23:36:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.static("dist"));
 app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms :obj")
 );
